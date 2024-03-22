1d538711022f2c0f234dbedcd1a50fce3115b4b7
mpj15 <michaeljns89@gmail.com>
2024-03-22 12:28:25 -0400

    Bumped python 3.8 rev to 3.19 in setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(name="geo_patrol",
       version=get_version(),
       packages=find_packages('src'),
       package_dir={'': 'src'},
-      python_requires="==3.8.18",
+      python_requires="==3.8.19",
       install_requires=[
         "numpy==1.21.6",
         "gym==0.21.0", #really should replace gym 0.21.0 or 0.22.0
